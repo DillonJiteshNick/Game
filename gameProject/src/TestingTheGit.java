ff4141a1322cb3b35732fa8254d10fc7213a6c79
nicholasdietrich <nicholasdietrich@10.17.194.1>
2017-01-11 14:51:29 -0500

    Changed...

--- gameProject/src/TestingTheGit.java
+++ gameProject/src/TestingTheGit.java
@@ -4,6 +4,9 @@ public class TestingTheGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello You Are Cool");
+
+
+System.out.println("Eat the money");
 	}
 
 }
