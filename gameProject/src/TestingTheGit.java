b079d87ac34847aa7c5731a0cf325787350831a4
Owner <Owner@DELL>
2017-01-11 14:57:25 -0500

    Changes

--- gameProject/src/TestingTheGit.java
+++ gameProject/src/TestingTheGit.java
@@ -3,10 +3,10 @@ public class TestingTheGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello You Are Cool");
+		System.out.println("Hello You Are Cool");
+		System.out.println("Thanks");
 
-
-System.out.println("Eat the money");
+		System.out.println("Eat the money");
 	}
 
 }
