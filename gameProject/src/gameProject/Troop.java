e5fa2b97aa8e6f0a6fa0eecbe3038e0e9997ce73
322954991 <322954991@C-3ZYDYR1.tdsb.on.ca>
2017-01-12 14:20:13 -0500

    updated troop class

--- gameProject/src/gameProject/Troop.java
+++ gameProject/src/gameProject/Troop.java
@@ -1,5 +1,21 @@
 package gameProject;
 
-public class Troop {
-
+public class Troop extends MovingObject {
+	private int health;
+	private int damage;
+	
+	public int getHealth() {
+		return health;
+	}
+	
+	public void setHealth(int decidedHealth) {
+		health=decidedHealth;
+	}
+	public int getDamage() {
+		return damage;
+	}
+	
+	public void setDamage(int decidedDamage) {
+		damage=decidedDamage;
+	}
 }
